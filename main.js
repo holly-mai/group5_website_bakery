19ed53ceee156cfbd08579d50bb14d2f4bb493d9
jessikatpr <116025190+jessikatpr@users.noreply.github.com>
2022-12-04 22:41:59 +0000

    carousel and bootstrap
    I made some changes on the code that might affect other styling or script code. I changed: - class “dots” to “dot” in index.html
    “circles” was changed to “dots”
    in the JS file I changed most of the code. The carousel changes picture every 4 seconds, when you hover the mouse it stops and when moveout the mouse it continues.
    I added a logo and a cart image(save in the file)
    the bootstrap link is now at the bottom of the index page ( this way it does not interferes with the images in the carousel.)

--- main.js
+++ main.js
@@ -1,42 +1,69 @@
-var slidePosition = 1;
-SlideShow(slidePosition);
+var slideIndex = 1;
 
-// forward/back controls
-function plusSlides(n) {
-    SlideShow(slidePosition += n);
+var myTimer;
+
+var slideshowContainer = document.getElementsByClassName('slideshow')[0]
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+  
+    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    ///slideshowContainer = document.getElementsByClassName('sldeshow')[0];
+  
+    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+  
+    slideshowContainer.addEventListener('mouseenter', pause)
+    slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  
+  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  }
 }
 
-// image controls
 function currentSlide(n) {
-    SlideShow(slidePosition =n);
+    clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  showSlides(slideIndex = n);    
+
 }
 
-function SlideShow(n) {
-    var i;
-    var slides = document.getElementsByClassName("Containers");
-    var circles = document.getElementsByClassName("dots");
-    if (n > slides.length) {slidePosition = 1}
-    if (n < 1) {slidePosition= slides.length}
-    for (i = 0; i <slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("containers");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
 
-for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace("enable", "");
-}
-
-slides[slidePosition-1].style.display = "block"
-circles[slidePosition-1].className += "enable";
-
-var slidePosition = 0;
-SlideShow();
-
-function SlideShow() {
-    var i;
-    var slides = document.getElementsByClassName("Containers");
-    for (i=0; i < slides.length; i++) {
-        slides[i].styles.display = "block";
-        setTimeout(SlideShow,2000); // change image every 2 seconds
-    }
-}
+pause = () => {
+    clearInterval(myTimer);
+  }
+  
+  resume = () =>{
+    clearInterval(myTimer);
+    myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
+  }
